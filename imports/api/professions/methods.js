2e737de79007d1b90abf8205f6abc0a9583c0987
GHOST home <lwhiteghostl@gmail.com>
2018-01-21 16:24:06 +0500

    Добавление/Удаление профессий в справочник

--- imports/api/professions/methods.js
+++ imports/api/professions/methods.js
@@ -15,7 +15,6 @@ export const insert = new ValidatedMethod({
     const profession = {
       name
     };
-
     Professions.insert(profession);
   },
 });
--- imports/ui/pages/ProfessionsPage.jsx
+++ imports/ui/pages/ProfessionsPage.jsx
@@ -9,6 +9,7 @@ import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table';
 import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
 
 import {
   insert,
@@ -18,18 +19,64 @@ import {
 
 export default class ProfessionsPage extends BaseComponent {
 
+  state = {
+    message: '',
+    open: false,
+  };
+
   constructor(props) {
     super(props);
     this.state = {...this.state, editing: undefined, open: false, value: "" };
   }
 
   onClickAddedButton = () => {
-    insert.call({name: this.state.value});
+    insert.call({name: this.state.value}, (error)=>{
+      this.setState({
+        open: true
+      });
+      if(error){
+        this.setState({
+          message: 'Ошибка при добавлении профессии'
+        });
+        return false;
+      }
+      this.setState({
+        message: 'Добавление прошло успешно.'
+      });
+      return true;
+    });
+    this.setState({
+      value: "",
+    })
   };
 
   handleChange = (event) => {
     this.setState({
       value: event.target.value,
+    })
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  onClickRemove = (profession) => {
+    remove.call({id: profession._id}, (error)=>{
+      this.setState({
+        open: true
+      });
+      if(error){
+        this.setState({
+          message: 'Ошибка при удалении профессии'
+        });
+        return false;
+      }
+      this.setState({
+        message: 'Удаление прошло успешно.'
+      });
+      return true;
     })
   };
 
@@ -41,7 +88,7 @@ export default class ProfessionsPage extends BaseComponent {
     let ProfessionsList = (
       <div>
         <Toolbar>
-          <ToolbarGroup firstChild={true} style={{marginLeft: 0}}>
+          <ToolbarGroup firstChild={true} style={{marginLeft: 0, width: '100%'}}>
             <TextField
               fullWidth={true}
               name={'newProfession'}
@@ -67,7 +114,7 @@ export default class ProfessionsPage extends BaseComponent {
                     <IconButton
                       tooltip="Изменить"
                       tooltipPosition='top-center'
-                      onClick={() => this.onEditingChange(profession, true)}
+                      onClick={() => this.onClickRemove(profession)}
                     >
                       <ContentRemove/>
                     </IconButton>
@@ -86,6 +133,12 @@ export default class ProfessionsPage extends BaseComponent {
           {loading
             ? <Message title={i18n.__('pages.ProfessionsPage.loading')} />
             : ProfessionsList}
+          <Snackbar
+              open={this.state.open}
+              message={this.state.message}
+              autoHideDuration={4000}
+              onRequestClose={this.handleRequestClose}
+          />
         </div>
       </div>
     );
